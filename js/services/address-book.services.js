7131b12769a7c513d5ef7f04256a99c1c2a5b4ee
nhftkfr25 <168108819+nhftkfr25@users.noreply.github.com>
2024-08-20 17:37:47 +0300

    Implemented basic functions to retrieve and process data needed for creating alien profiles.
    Implemented basic functions to retrieve and process data needed for creating alien profiles.

--- js/services/address-book.services.js
+++ js/services/address-book.services.js
@@ -1,1 +1,48 @@
 'use strict'
+
+let aliens = []
+
+function onInit() {
+  menuRender()
+
+  getAns(getAliensList)
+}
+
+function getAns(cb) {
+  const xhr = new XMLHttpRequest()
+
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+      const ans = JSON.parse(xhr.responseText)
+      cb(ans)
+    }
+  }
+  xhr.open(
+    'GET',
+    'http://www.filltext.com/?rows=30&fname=%7bfirstName%7d&lname=%7blastName%7d&tel=%7bphone%7Cformat%7d&address=%7bstreetAddress%7d&city=%7bcity%7d&state=%7busState%7Cabbr%7d&zip=%7bzip%7d&pretty=true',
+    true
+  )
+  xhr.send()
+}
+
+function getAliens() {
+  return aliens
+}
+
+function setAliens(aliensList) {
+  aliens = aliensList
+}
+
+function getAliensList(aliensList) {
+  setAliens(initAliensIds(aliensList))
+  console.log(getAliens())
+}
+
+function initAliensIds(aliensList) {
+  aliensList.map((alien) => {
+    alien['id'] = generateUniqueId()
+    alien['image'] = `https://robohash.org/${alien['id']}?set=set5`
+  })
+
+  return aliensList
+}
