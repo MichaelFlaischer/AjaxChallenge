0daec6fef0a6a5ac81d1ac82b6bb4751ff8ce577
nhftkfr25 <168108819+nhftkfr25@users.noreply.github.com>
2024-08-27 11:42:17 +0300

    Implemented additional API calls to display a joke if the answer is YES or a random dog image if the answer is NO, after rendering the initial response.
    Implemented additional API calls to display a joke if the answer is YES or a random dog image if the answer is NO, after rendering the initial response.

--- js/controller/yes-no.controller.js
+++ js/controller/yes-no.controller.js
@@ -1,5 +1,10 @@
 'use strict'
 
+let answersToLoad = 2
+let count = 0
+let currentAnswer = null
+let currentExtraInfo = null
+
 function onInit() {
   const elInputField = document.querySelector('.user-question')
   elInputField.addEventListener('input', debounce(handleQuestion, 300))
@@ -8,27 +13,63 @@ function onInit() {
 }
 
 function displayAnswer(answer) {
-  const elResponseSection = document.querySelector('.response-section')
+  currentAnswer = answer
+  const elResponseSection = document.querySelector('.response')
+  const img = new Image()
+
+  img.src = answer.image
+  img.onload = () => {
+    count++
+    checkIfReadyToDisplay()
+  }
+
+  fetchExtraInfo(answer.answer, handleExtraInfoLoad)
+}
+
+function handleExtraInfoLoad(extraInfo) {
+  currentExtraInfo = extraInfo
+  if (extraInfo.setup && extraInfo.punchline) {
+    count++
+    checkIfReadyToDisplay()
+  } else if (extraInfo.message) {
+    const extraImg = new Image()
+    extraImg.src = extraInfo.message
+    extraImg.onload = () => {
+      count++
+      checkIfReadyToDisplay()
+    }
+  }
+}
+
+function displayAnswerContent() {
+  const elResponseSection = document.querySelector('.response')
+  const extraInfoElement = document.querySelector('.extra-info')
 
   elResponseSection.innerHTML = `
-          <p class="answer" data-i18n="answerText">${answer.answer}</p>
-          <img class="response-image" src="${answer.image}" alt="Response Image" data-i18n="responseImageAlt" />
+    <p class="answer" data-i18n="answerText">${currentAnswer.answer}</p>
+    <img class="response-image" src="${currentAnswer.image}" alt="Response Image" data-i18n="responseImageAlt" />
+  `
 
-          <p class="extra-info" data-i18n="extraInfoText"></p>
-        `
+  if (currentExtraInfo.setup && currentExtraInfo.punchline) {
+    extraInfoElement.innerText = `${currentExtraInfo.setup} - ${currentExtraInfo.punchline}`
+  } else if (currentExtraInfo.message) {
+    extraInfoElement.innerHTML = `<img src="${currentExtraInfo.message}" alt="Random Dog" />`
+  }
 }
 
 function showLoader() {
-  const elResponseSection = document.querySelector('.response-section')
+  const elResponseSection = document.querySelector('.response')
   elResponseSection.innerHTML = `
         <div class="loader-container">
           <img src="img/loader.svg" alt="Loading..." class="loader-img" data-i18n="loading" />
         </div>`
 }
 
-function hideLoader() {
-  const loaderContainer = document.querySelector('.loader-container')
-  if (loaderContainer) {
-    loaderContainer.remove()
-  }
+function clearPreviousResponse() {
+  answersToLoad = 2
+  count = 0
+  const elResponseSection = document.querySelector('.response')
+  const extraInfoElement = document.querySelector('.extra-info')
+  elResponseSection.innerHTML = ''
+  extraInfoElement.innerHTML = ''
 }
--- js/services/yes-no.services.js
+++ js/services/yes-no.services.js
@@ -3,6 +3,7 @@
 function handleQuestion(event) {
   const question = event.target.value
   if (question.length >= 3 && question.endsWith('?')) {
+    clearPreviousResponse()
     showLoader()
     fetchYesNoAnswer(question, displayAnswer)
   }
@@ -10,4 +11,22 @@ function handleQuestion(event) {
 
 function fetchYesNoAnswer(question, cb) {
   $.get('https://yesno.wtf/api', cb)
+}
+
+function fetchExtraInfo(answer, cb) {
+  let apiUrl = ''
+
+  if (answer === 'yes') {
+    apiUrl = 'https://official-joke-api.appspot.com/random_joke'
+  } else if (answer === 'no') {
+    apiUrl = 'https://dog.ceo/api/breeds/image/random'
+  }
+
+  $.get(apiUrl, cb)
+}
+
+function checkIfReadyToDisplay() {
+  if (count === answersToLoad) {
+    displayAnswerContent()
+  }
 }
